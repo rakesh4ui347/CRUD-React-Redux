6cce45946f1c10fb5746dd7553fc4187be560d06
Rakesh Kumar <39724733+rakesh4ui347@users.noreply.github.com>
2019-11-13 11:13:05 +0530

    Create Post.js

---
+++ src/Post.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+class Post extends  Component{
+    render(){
+        return (
+            <div className="post">
+                
+            </div>
+        )
+    }
+}
+
+export default (Post);
