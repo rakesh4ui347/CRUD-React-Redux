9af7fac2cb6ebb7e5f90689e6ce2297da46fc639
Rakesh Kumar <39724733+rakesh4ui347@users.noreply.github.com>
2019-11-18 12:28:59 +0530

    Update AllPost.js

--- src/AllPost.js
+++ src/AllPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 class AllPost extends Component {
     render() {
@@ -12,5 +12,9 @@ class AllPost extends Component {
     }
 }
 
-
-export default AllPost;
+const mapStateToProps = (state) => {
+    return {
+        posts: state
+    }
+}
+export default connect(mapStateToProps)(AllPost);
